f51ca66424eec2be0e95dcd43960df81ba0f0a27
_simran_1031 <54627904+Simranjakhodia@users.noreply.github.com>
2021-07-02 13:03:39 +0530

    gfg

---
+++ Recursion/factorial.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int);
+
+int main(){
+
+	int n,result;
+	cout<<"Enter a non-negative no." ;
+	cin >> n;
+	result = factorial(n);
+	cout <<"Factorial of "<<n<<" is "<<result<<endl;
+	return 0;
+}
+
+int factorial(int n){
+	if(n>1)
+	return n*factorial(n-1);
+	else 
+	return 1;
+}
+
