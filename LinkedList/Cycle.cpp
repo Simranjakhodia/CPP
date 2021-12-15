09837766cbada348427570b64f4a865d9a064133
_simran_1031 <54627904+Simranjakhodia@users.noreply.github.com>
2021-12-16 01:15:22 +0530

    Create Cycle.cpp

---
+++ LinkedList/Cycle.cpp
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/linked-list-cycle-ii/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if(!head) return NULL;
+        ListNode *slow = head, *fast = head, *entry = head;
+        while(fast->next && fast->next->next){
+            slow = slow -> next;
+            fast = fast -> next -> next;
+            if(slow == fast) {
+               while(slow != entry){
+                   slow = slow -> next;
+                   entry = entry -> next;
+               } 
+                return entry;
+            }
+        }
+        return NULL;
+    }
+};
