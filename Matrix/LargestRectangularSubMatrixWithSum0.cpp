f7a486a8ea0372f5e3ac5c6b9f464f3bde53a479
_simran_1031 <54627904+Simranjakhodia@users.noreply.github.com>
2022-09-01 22:03:13 +0530

    Create LargestRectangularSubMatrixWithSum0.cpp

---
+++ Matrix/LargestRectangularSubMatrixWithSum0.cpp
@@ -0,0 +1,61 @@
+// Largest rectangular sub-matrix whose sum is 0
+https://practice.geeksforgeeks.org/problems/largest-rectangular-sub-matrix-whose-sum-is-0/1
+
+
+class Solution{
+  public:
+  vector<vector<int>> sumZeroMatrix(vector<vector<int>> a){
+      int r = a.size(), c = a[0].size();
+      
+      int left, right, up, down;
+      left = right = up = down = 0;
+      
+      for(int i = 0; i < c; i++){
+          vector<int> v(r, 0);
+          
+          for(int j = i; j < c; j++){
+              for(int k = 0; k < r; k++) 
+                  v[k] += a[k][j];
+              
+              unordered_map<long long, int> mp;
+              mp[0] = -1;
+              
+              int leftv = 0, rightv = 0;
+              long long sum = 0;
+              
+              for(int k = 0; k < r; k++) {
+                  sum += v[k];
+                  
+                  if(mp.count(sum)) {
+                      if((k - mp[sum]) > (rightv - leftv + 1)) {
+                          rightv = k;
+                          leftv = mp[sum]+1;
+                      }
+                  }
+                  else 
+                        mp[sum] = k;
+              }
+              
+              if((j - i + 1)*(rightv - leftv + 1) > (right - left + 1) * (down - up + 1)) {
+                  up = leftv;
+                  down = rightv;
+                  left = i;
+                  right = j;
+              }
+          }
+      }
+      vector<vector<int>> res;
+      for(int i = up; i <= down; i++) {
+          vector<int> arr;
+          for(int j = left; j <= right; j++) {
+              arr.push_back(a[i][j]);
+          }
+          res.push_back(arr);
+      }
+      return res;
+  }
+};
+
+
+
+
