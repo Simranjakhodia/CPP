53420f0a988f26184e0954cab63d4b38060fb43e
_simran_1031 <54627904+Simranjakhodia@users.noreply.github.com>
2022-09-07 23:08:18 +0530

    Update Maximum-path-sum-two-leaves-binary-tree.cpp

--- Dynamic Programming  problems/Maximum-path-sum-two-leaves-binary-tree.cpp
+++ Dynamic Programming  problems/Maximum-path-sum-two-leaves-binary-tree.cpp
@@ -14,66 +14,6 @@ struct Node {
     }
 };
 
-// Function to Build Tree
-Node *buildTree(string str) {
-    // Corner Case
-    if (str.length() == 0 || str[0] == 'N') return NULL;
-
-    // Creating vector of strings from input
-    // string after spliting by space
-    vector<string> ip;
-
-    istringstream iss(str);
-    for (string str; iss >> str;) ip.push_back(str);
-
-    // Create the root of the tree
-    Node *root = new Node(stoi(ip[0]));
-
-    // Push the root to the queue
-    queue<Node *> queue;
-    queue.push(root);
-
-    // Starting from the second element
-    int i = 1;
-    while (!queue.empty() && i < ip.size()) {
-
-        // Get and remove the front of the queue
-        Node *currNode = queue.front();
-        queue.pop();
-
-        // Get the current Node's value from the string
-        string currVal = ip[i];
-
-        // If the left child is not null
-        if (currVal != "N") {
-
-            // Create the left child for the current Node
-            currNode->left = new Node(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->left);
-        }
-
-        // For the right child
-        i++;
-        if (i >= ip.size()) break;
-        currVal = ip[i];
-
-        // If the right child is not null
-        if (currVal != "N") {
-
-            // Create the right child for the current Node
-            currNode->right = new Node(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->right);
-        }
-        i++;
-    }
-
-    return root;
-}
-
 
 // } Driver Code Ends
 /*
@@ -143,18 +83,9 @@ leaf node is that node that has zero child
 but  the root itself has one child in this case 
 */
 
-//{ Driver Code Starts.
+https://practice.geeksforgeeks.org/problems/maximum-path-sum/1
+https://leetcode.com/problems/binary-tree-maximum-path-sum/
+https://leetcode.com/problems/binary-tree-maximum-path-sum/
+https://leetcode.com/problems/path-sum/
+https://leetcode.com/problems/path-sum-ii/
 
-int main() {
-    int tc;
-    scanf("%d ", &tc);
-    while (tc--) {
-        string treeString;
-        getline(cin, treeString);
-        Node *root = buildTree(treeString);
-        Solution ob;
-        cout << ob.maxPathSum(root) << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
